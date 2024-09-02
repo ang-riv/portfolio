155b0d222d108609082ab5c8df438879c9f63020
a.rivera <a.riveraa99@gmail.com>
2024-09-02 10:57:44 -0500

    Added fonts, and iconify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
-
+import "./style.css";
+import { Icon } from "@iconify/react";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Main Heading Text</h1>
+      <h2>Subheading Text</h2>
+      <p>Paragraph Text</p>
+      <Icon icon="mdi-light:home" className="home-icon" />
+    </div>
+  );
 }
 
 export default App;
