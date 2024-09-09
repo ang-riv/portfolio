351b50f65db3d392fe23cabb76eb649bdaebb8b6
a.rivera <a.riveraa99@gmail.com>
2024-09-09 10:47:54 -0500

    Created layout for one section of about

--- src/AboutSection.js
+++ src/AboutSection.js
@@ -18,8 +18,43 @@ const AboutSection = React.forwardRef((props, ref) => {
             outline: "1px dotted lightBlue",
             height: "90%",
             width: "70%",
+            display: "flex",
+            flexDirect: "row",
           }}
-        ></div>
+        >
+          <div
+            style={{
+              outline: "1px dotted red",
+              backgroundColor: "salmon",
+              width: "50%",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "3%",
+            }}
+          >
+            <div
+              style={{ backgroundColor: "red", height: "62%", width: "90%" }}
+            ></div>
+            <div
+              style={{
+                backgroundColor: "purple",
+                height: "30%",
+                width: "90%",
+              }}
+            ></div>
+          </div>
+          <div
+            style={{
+              outline: "1px dotted green",
+              backgroundColor: "lightGreen",
+              width: "50%",
+              height: "100%",
+            }}
+          ></div>
+        </div>
       </div>
     </section>
   );
