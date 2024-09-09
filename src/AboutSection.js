9a93025b65d065f6ee7cbb7495f5c94ed6b5cf92
a.rivera <a.riveraa99@gmail.com>
2024-09-09 11:59:14 -0500

    Added box shadows and content styles for inside the tiles

--- src/AboutSection.js
+++ src/AboutSection.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 const AboutSection = React.forwardRef((props, ref) => {
+  // each tiles innerStyle
+  const innerStyle = {};
   return (
     <section ref={ref} style={{ height: "85vh" }}>
       <h2>About</h2>
@@ -34,20 +36,46 @@ const AboutSection = React.forwardRef((props, ref) => {
           >
             <div
               style={{
-                backgroundColor: "red",
+                backgroundColor: "white",
+                border: "1px solid #b5b5b5",
                 height: "57%",
                 width: "95%",
                 borderRadius: "10px",
+                boxShadow: "0px 4px 10px 0px #b5b5b5",
               }}
             ></div>
             <div
               style={{
-                backgroundColor: "purple",
+                backgroundColor: "#CCFFCC",
                 height: "35%",
                 width: "95%",
                 borderRadius: "10px",
+                boxShadow: "0px 4px 10px 0px #b5b5b5",
               }}
-            ></div>
+            >
+              <div style={{ margin: "15px" }}>
+                <h3
+                  style={{
+                    margin: "0 0 10px 0",
+                    padding: "0",
+                  }}
+                >
+                  Title
+                </h3>
+                <p
+                  style={{
+                    margin: "0",
+                    padding: "0",
+                  }}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco.
+                </p>
+              </div>
+            </div>
           </div>
           {/* section 2 */}
           <div
@@ -63,28 +91,97 @@ const AboutSection = React.forwardRef((props, ref) => {
           >
             <div
               style={{
-                backgroundColor: "pink",
+                backgroundColor: "#FFAFAF",
                 height: "25%",
                 width: "90%",
                 borderRadius: "10px",
+                boxShadow: "0px 4px 10px 0px #b5b5b5",
               }}
-            ></div>
+            >
+              <div style={{ margin: "15px" }}>
+                <h3
+                  style={{
+                    margin: "0 0 10px 0",
+                    padding: "0",
+                  }}
+                >
+                  Title
+                </h3>
+                <p
+                  style={{
+                    margin: "0",
+                    padding: "0",
+                  }}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud.
+                </p>
+              </div>
+            </div>
             <div
               style={{
-                backgroundColor: "blue",
+                backgroundColor: "#CCCCFF",
                 height: "39%",
                 width: "90%",
                 borderRadius: "10px",
+                boxShadow: "0px 4px 10px 0px #b5b5b5",
               }}
-            ></div>
+            >
+              <div style={{ margin: "15px" }}>
+                <h3
+                  style={{
+                    margin: "0 0 10px 0",
+                    padding: "0",
+                  }}
+                >
+                  Title
+                </h3>
+                <p
+                  style={{
+                    margin: "0",
+                    padding: "0",
+                  }}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco. Ut enim ad
+                  minim veniam, quis nostrud.
+                </p>
+              </div>
+            </div>
             <div
               style={{
-                backgroundColor: "olive",
+                backgroundColor: "#F2E589",
                 height: "25%",
                 width: "90%",
                 borderRadius: "10px",
+                boxShadow: "0px 4px 10px 0px #b5b5b5",
               }}
-            ></div>
+            >
+              <div style={{ margin: "15px" }}>
+                <h3
+                  style={{
+                    margin: "0 0 10px 0",
+                    padding: "0",
+                  }}
+                >
+                  Title
+                </h3>
+                <p
+                  style={{
+                    margin: "0",
+                    padding: "0",
+                  }}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
