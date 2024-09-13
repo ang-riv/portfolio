0ed52c1a63a53570f65ac98b3d9a696676e1f26d
a.rivera <a.riveraa99@gmail.com>
2024-09-13 13:59:50 -0500

    Started on Projects Section, created project tile component

--- src/ProjectTile.js
+++ src/ProjectTile.js
@@ -5,8 +5,8 @@ function ProjectTile(props) {
   return (
     <div
       style={{
-        height: "390px",
-        width: "280px",
+        height: "350px",
+        width: "250px",
         border: "1px solid #D6D6D6",
         borderRadius: "5px",
         padding: "1%",
@@ -22,7 +22,7 @@ function ProjectTile(props) {
       ></div>
       <div style={{ margin: "0 5px" }}>
         <h4>Project Name</h4>
-        <p style={{ marginBottom: "11px" }}>
+        <p style={{ margin: "10px 0" }}>
           Short paragraph that has like one to three sentences. Cool sentence
           here. Super cool sentence.
         </p>
--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -3,19 +3,30 @@ import ProjectTile from "./ProjectTile";
 
 const ProjectsSection = React.forwardRef((props, ref) => {
   return (
-    <section ref={ref} style={{ height: "85vh" }}>
+    <section ref={ref} style={{ height: "90vh" }}>
       <h2>Projects</h2>
       <div
         className="content-wrapper"
         style={{
           height: "100%",
           width: "100%",
-          outline: "1px dotted green",
           display: "flex",
           justifyContent: "center",
         }}
       >
-        <div style={{ width: "80%", outline: "1px dotted grey" }}>
+        <div
+          style={{
+            width: "90%",
+            display: "flex",
+            flexFlow: "row wrap",
+            justifyContent: "center",
+            gap: "5px 25px",
+          }}
+        >
+          <ProjectTile title="" description="" img="" color="" />
+          <ProjectTile />
+          <ProjectTile />
+          <ProjectTile />
           <ProjectTile />
         </div>
       </div>
