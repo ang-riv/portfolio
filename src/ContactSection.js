6bd75ce939fb151e8b69833d0f0cce7f530b40bf
a.rivera <a.riveraa99@gmail.com>
2024-09-18 12:33:37 -0500

    Added social links in contact section

--- src/ContactSection.js
+++ src/ContactSection.js
@@ -1,33 +1,14 @@
 import React from "react";
-
+import { SocialLinks } from "./Icons";
 const ContactSection = React.forwardRef((props, ref) => {
   const outline = {
-    width: "45%",
+    width: "100%",
+    height: "100%",
     backgroundColor: "#9FD1FF",
   };
   const titleStyle = {
     textAlign: "left",
-    padding: "0 0 0 5%",
-  };
-  const semiCircle = {
-    width: "10rem",
-    height: "5rem",
-    backgroundColor: "#9FD1FF",
-    borderRadius: "5rem 5rem 0 0",
-    transform: "rotate(90deg)",
-    position: "relative",
-    bottom: "56%",
-    left: "91%",
-  };
-  const semiCircle2 = {
-    width: "10rem",
-    height: "5rem",
-    backgroundColor: "white",
-    borderRadius: "5rem 5rem 0 0",
-    transform: "rotate(90deg)",
-    position: "relative",
-    left: "-9%",
-    top: "36%",
+    padding: "2% 0 0 5%",
   };
   return (
     <section
@@ -35,17 +16,31 @@ const ContactSection = React.forwardRef((props, ref) => {
       style={{
         paddingTop: "3em",
         height: "40vh",
-        display: "flex",
-        justifyContent: "space-between",
       }}
     >
       <div style={outline}>
         <h2 style={titleStyle}>Let's Connect</h2>
-        <div style={{ outline: "1px dotted red", height: "60%" }}></div>
-        <div style={semiCircle}></div>
-      </div>
-      <div style={outline}>
-        <div style={semiCircle2}></div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            height: "70%",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              marginTop: "1%",
+              height: "80%",
+              width: "70%",
+              display: "flex",
+              flexFlow: "row wrap",
+              justifyContent: "space-around",
+            }}
+          >
+            <SocialLinks />
+          </div>
+        </div>
       </div>
     </section>
   );
