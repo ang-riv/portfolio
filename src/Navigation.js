1e7c2e3c0cb910112c977e70ecd95571dd9fc88f
a.rivera <a.riveraa99@gmail.com>
2024-09-19 11:17:31 -0500

    Moved navigation styles to css file

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,53 +8,14 @@ const Navigation = ({ aboutRef, skillsRef, projectsRef, contactRef }) => {
     }
   };
 
-  // TODO: move all of these into the css file
-  const containerStyles = {
-    "margin-top": "1em",
-    height: "3em",
-    display: "flex",
-    "justify-content": "end",
-    "align-items": "center",
-  };
-
-  const ulStyle = {
-    display: "flex",
-    "justify-content": "end",
-    "align-items": "center",
-  };
-  const linkStyles = {
-    marginRight: "3em",
-    fontWeight: "bolder",
-    fontSize: "1.3em",
-    "list-style-type": "none",
-  };
-
-  const contactStyle = {
-    backgroundColor: "#9FD1FF",
-    marginRight: "2.5em",
-    padding: "0.25em 1.3em",
-    borderRadius: "0.938em",
-    fontWeight: "bolder",
-    fontSize: "1.3em",
-    listStyleType: "none",
-  };
-
   return (
-    <div style={containerStyles}>
+    <div className="navContainer">
       <nav>
-        <ul style={ulStyle}>
-          <li style={linkStyles} onClick={() => scrollToSection(aboutRef)}>
-            About
-          </li>
-          <li style={linkStyles} onClick={() => scrollToSection(skillsRef)}>
-            Skills
-          </li>
-          <li style={linkStyles} onClick={() => scrollToSection(projectsRef)}>
-            Projects
-          </li>
-          <li style={contactStyle} onClick={() => scrollToSection(contactRef)}>
-            Contact
-          </li>
+        <ul>
+          <li onClick={() => scrollToSection(aboutRef)}>About</li>
+          <li onClick={() => scrollToSection(skillsRef)}>Skills</li>
+          <li onClick={() => scrollToSection(projectsRef)}>Projects</li>
+          <li onClick={() => scrollToSection(contactRef)}>Contact</li>
         </ul>
       </nav>
     </div>
