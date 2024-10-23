a5c41b3cd1afb9e53ea5e0c843c5095be0e505d3
a.rivera <a.riveraa99@gmail.com>
2024-10-23 11:57:50 -0500

    Destructured props for each project tile

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -5,52 +5,75 @@ import rogue from "./assets/project-imgs/rogue-pickings.png";
 import gallery from "./assets/project-imgs/gh-gallery.png";
 import notes from "./assets/project-imgs/sticky-notes.png";
 import ProjectTile from "./ProjectTile.js";
+import { globalColors } from "./Imports.js";
 
 const ProjectsSection = React.forwardRef((props, ref) => {
+  // individual project info + props
+  const project1Props = {
+    title: "Unplugged",
+    description:
+      "A multi-page website about Unplugged, a work retreat. Created with a mobile-first design in mind and with Flexbox and media queries.",
+    img: unplugged,
+    color: globalColors.blue,
+    link: "https://ang-riv.github.io/unplugged/",
+    imgAlt: "image preview of work retreat website",
+  };
+
+  const project2Props = {
+    title: "Potluck Generator",
+    description:
+      "A website that takes guest names and assigns them a random recipe. Uses React Bootstrap for styling and the spoonacular API for the random recipes.",
+    img: potluck,
+    color: globalColors.pink,
+    link: "",
+    imgAlt: "image preview of potluck recipe generator website",
+  };
+
+  const project3Props = {
+    title: "Github Repo Gallery",
+    description:
+      "A personalized website that showcases all my Github Repos. Want more information? Click on each repo to be taken to my Github.",
+    img: gallery,
+    color: globalColors.yellow,
+    link: "https://ang-riv.github.io/gh-repo-gallery/",
+    imgAlt: "image preview of github repo gallery",
+  };
+
+  const project4Props = {
+    title: "Sticky Note App",
+    description:
+      "An app that allows users to write on sticky notes and track of all their to-dos. Created with React and features a dynamic search bar to look for sticky notes with the entered word(s).",
+    img: notes,
+    color: globalColors.green,
+    imgAlt: "image preview of sticky note application",
+  };
+
+  const project5Props = {
+    title: "Rogue Pickings",
+    description:
+      "A single-page website for a food truck called Rogue Pickings. Focuses on being responsive through the use of Flexbox and responsive typography.",
+    img: rogue,
+    color: globalColors.purple,
+    link: "https://ang-riv.github.io/responsive-rouge-pickings/",
+    imgAlt: "image preview of a website for a food truck",
+  };
+
+  const projectArr = [
+    project1Props,
+    project2Props,
+    project3Props,
+    project4Props,
+    project5Props,
+  ];
+
   return (
     <section ref={ref} style={{ height: "fit-content" }}>
       <h2>Projects</h2>
       <div className="project-content-wrapper">
         <div className="project-container">
-          <ProjectTile
-            title="Unplugged"
-            description="A multi-page website about Unplugged, a work retreat. Created with a mobile-first design in mind and with Flexbox and media queries."
-            img={unplugged}
-            color="#9FD1FF"
-            link="https://ang-riv.github.io/unplugged/"
-            imgAlt="image preview of work retreat website"
-          />
-          <ProjectTile
-            title="Potluck Generator"
-            description="A website that takes guest names and assigns them a random recipe. Uses React Bootstrap for styling and the spoonacular API for the random recipes."
-            img={potluck}
-            color="#FFAFAF"
-            link=""
-            imgAlt="image preview of potluck recipe generator website"
-          />
-          <ProjectTile
-            title="Github Repo Gallery"
-            description="A personalized website that showcases all my Github Repos. Want more information? Click on each repo to be taken to my Github."
-            img={gallery}
-            color="#F2E589"
-            link="https://ang-riv.github.io/gh-repo-gallery/"
-            imgAlt="image preview of github repo gallery"
-          />
-          <ProjectTile
-            title="Sticky Note App"
-            description="An app that allows users to write on sticky notes and track of all their to-dos. Created with React and features a dynamic search bar to look for sticky notes with the entered word(s)."
-            img={notes}
-            color="#CCFFCC"
-            imgAlt="image preview of sticky note application"
-          />
-          <ProjectTile
-            title="Rogue Pickings"
-            description="A single-page website for a food truck called Rogue Pickings. Focuses on being responsive through the use of Flexbox and responsive typography."
-            img={rogue}
-            color="#BCBCFF"
-            link="https://ang-riv.github.io/responsive-rouge-pickings/"
-            imgAlt="image preview of a website for a food truck"
-          />
+          {projectArr.map((project) => (
+            <ProjectTile {...project} />
+          ))}
         </div>
       </div>
     </section>
