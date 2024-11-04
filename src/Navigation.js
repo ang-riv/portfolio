98575ca08cd1ff7cbcf7f93ef10061a15d3aedb7
a.rivera <a.riveraa99@gmail.com>
2024-11-04 12:20:34 -0600

    Added hover effects to navigation contact background

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,9 +4,23 @@ import useWindowSize from "./useWindowSize";
 import { QuestionMarkIcon, GearIcon, CodeIcon, SpeechIcon } from "./Icons";
 
 const Navigation = ({ aboutRef, skillsRef, projectsRef, contactRef }) => {
-  // hover effects
+  // hover effects to check which element is being hovered
   const [isHovered, setIsHovered] = useState("");
 
+  // contact background style + hover effects
+  const contactStyle = {
+    width: "calc-size(fit-content, size + 25px)",
+    textAlign: "center",
+    position: "relative",
+    top: "3px",
+    backgroundColor: "var(--blue)",
+    marginRight: "2.5em",
+    padding: " 0.25em 1.3em",
+    borderRadius: "0.938em",
+    fontWeight: "bolder",
+    fontSize: "1.3em",
+    listStyleType: "none",
+  };
   // scrolling to a specific section using refs
   const scrollToSection = (ref) => {
     if (ref.current) {
@@ -37,27 +51,44 @@ const Navigation = ({ aboutRef, skillsRef, projectsRef, contactRef }) => {
         <div className="nav-container">
           <nav>
             <ul>
-              {refCopy.map((section) => (
-                <motion.li
-                  style={styles}
-                  onClick={() => scrollToSection(section.ref)}
-                  onHoverStart={() => setIsHovered(section.title)}
-                  onHoverEnd={() => setIsHovered("")}
-                >
-                  {section.title}
-                  <motion.div
-                    style={{
-                      height: "3px",
-                      width: "100%",
-                      backgroundColor: "#9fd1ff",
-                      transformOrigin: "left",
+              {refCopy.map((section) =>
+                section.title !== "Contact" ? (
+                  <motion.li
+                    style={styles}
+                    key={section.title}
+                    onClick={() => scrollToSection(section.ref)}
+                    onHoverStart={() => setIsHovered(section.title)}
+                    onHoverEnd={() => setIsHovered("")}
+                  >
+                    {section.title}
+                    <motion.div
+                      style={{
+                        height: "3px",
+                        width: "100%",
+                        backgroundColor: "#9fd1ff",
+                        transformOrigin: "left",
+                      }}
+                      initial={false}
+                      animate={{ scaleX: isHovered === section.title ? 1 : 0 }}
+                      transition={{ duration: 0.4 }}
+                    />
+                  </motion.li>
+                ) : (
+                  <motion.li
+                    style={contactStyle}
+                    key={section.title}
+                    onClick={() => scrollToSection(section.ref)}
+                    whileHover={{
+                      backgroundColor: [
+                        "hsl(208.8, 100, 81.2)",
+                        "hsl(208.5, 100, 56.3)",
+                      ],
                     }}
-                    initial={false}
-                    animate={{ scaleX: isHovered === section.title ? 1 : 0 }}
-                    transition={{ duration: 0.4 }}
-                  />
-                </motion.li>
-              ))}
+                  >
+                    {section.title}
+                  </motion.li>
+                )
+              )}
             </ul>
           </nav>
         </div>
@@ -65,7 +96,10 @@ const Navigation = ({ aboutRef, skillsRef, projectsRef, contactRef }) => {
         <div className="mobile-nav-container">
           <ul>
             {refCopy.map((section) => (
-              <li onClick={() => scrollToSection(section.ref)}>
+              <li
+                key={section.title}
+                onClick={() => scrollToSection(section.ref)}
+              >
                 {section.icon}
                 <p>{section.title}</p>
               </li>
