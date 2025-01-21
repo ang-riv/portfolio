353ec43830f9e6feaeb85c987d2389bcd3d67fe3
a.rivera <a.riveraa99@gmail.com>
2025-01-21 15:23:14 -0600

    Condensed refs

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import "./style.css";
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Navigation from "./Navigation.js";
 import IntroSection from "./IntroSection.js";
 import AboutSection from "./AboutSection.js";
 import SkillsSection from "./SkillsSection.js";
 import ProjectsSection from "./ProjectsSection.js";
 import ContactSection from "./ContactSection.js";
-import TestPage from "./TestPage.js";
+import { object } from "framer-motion/client";
+//import TestPage from "./TestPage.js";
 
 function App() {
-  const animateTest = false;
-  const introRef = useRef(null);
-  const aboutRef = useRef(null);
-  const skillsRef = useRef(null);
-  const projectsRef = useRef(null);
-  const contactRef = useRef(null);
+  const sectionTitles = ["intro", "skills", "about", "projects", "contact"];
+
+  const sectionRefs = useRef(
+    Object.fromEntries(sectionTitles.map((key) => [key, React.createRef()]))
+  );
+
+  const refProps = {
+    introRef: sectionRefs.current.intro,
+    aboutRef: sectionRefs.current.about,
+    skillsRef: sectionRefs.current.skills,
+    projectsRef: sectionRefs.current.projects,
+    contactRef: sectionRefs.current.contact,
+  };
 
   return (
     <div className="App">
-      {animateTest ? (
-        <TestPage />
-      ) : (
-        <>
-          <Navigation
-            aboutRef={aboutRef}
-            skillsRef={skillsRef}
-            projectsRef={projectsRef}
-            contactRef={contactRef}
-          />
-          <IntroSection ref={introRef} />
-          <SkillsSection ref={skillsRef} />
-          <AboutSection ref={aboutRef} />
-          <ProjectsSection ref={projectsRef} />
-          <ContactSection ref={contactRef} />
-        </>
-      )}
+      <Navigation {...refProps} />
+      <IntroSection ref={refProps.introRef} />
+      <SkillsSection ref={refProps.skillsRef} />
+      <AboutSection ref={refProps.aboutRef} />
+      <ProjectsSection ref={refProps.projectsRef} />
+      <ContactSection ref={refProps.contactRef} />
     </div>
   );
 }
